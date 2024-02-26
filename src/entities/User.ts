faeb133cc2f4d1197294a0f096321721dd212624
matthewfairman <156474555+matthewfairman@users.noreply.github.com>
2024-02-25 22:24:14 -0600

    test to own branch

---
+++ src/entities/User.ts
@@ -0,0 +1,46 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany, Relation } from 'typeorm';
+
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  passwordHash: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+
+  @Column({default: ""})
+  biography: string
+
+  @Column({default: username})
+  displayName
+
+  @Column({unique: true})
+  email: string
+
+  @Column({default: false})
+  verifiedEmail: boolean
+
+  // need to add profile picture functionality
+  @Column()
+  profilePic: string
+
+  // Relationships
+
+
+/*
+  friends 		many-many
+personalCalendar	one-one
+joinedEvents		many-many
+ownedEvents		one-many
+Commenter		one-many
+targetedUser		one-many
+RequestingUser		one-many 
+*/
+}
