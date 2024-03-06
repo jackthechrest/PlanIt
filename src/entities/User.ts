a4555babfdd8a09f026de3509a1f1c8f89dc0ba5
jackthechrest <jackson.chrestma@smail.astate.edu>
2024-03-06 11:31:38 -0600

    follow and verifycode entities

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-//, OneToMany, ManyToOne, ManyToMany, Relation
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Relation, OneToOne } from 'typeorm';
+import { Follow } from './Follow';
+import { VerifyCode } from './VerifyCode';
 
 @Entity()
 export class User {
@@ -35,9 +36,20 @@ export class User {
   // need to add profile picture functionality
   @Column()
   profilePic: string
+*/
 
   // Relationships
-*/
+  // Follow
+  @OneToMany(() => Follow, (follow) => follow.requestingUser, { cascade: ['insert', 'update'] })
+  following: Relation<Follow>[];
+
+  @OneToMany(() => Follow, (follow) => follow.targetedUser, { cascade: ['insert', 'update'] })
+  followers: Relation<Follow>[];
+
+  // Email Verification
+  @OneToOne(() => VerifyCode, (code) => code.user, { cascade: ['insert', 'update'] })
+  code: Relation<VerifyCode>;
+
 
 /*
   friends 		many-many
